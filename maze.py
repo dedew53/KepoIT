19d9229afc04e600dc301149e9ab26af60453f70
ishaq_fakhrozi <37141145+Newtton@users.noreply.github.com>
2018-03-25 19:46:17 +0700

    maze fix

--- maze.py
+++ maze.py
@@ -1,69 +1,72 @@
-N=8
-def printSolution(sol):
-         for i in sol:
-                 for j in i:
-                         print(str(j) + " ", end="")
-                 print("")
+#ukuran matrik
+N = 8
+#cetak langkah jalurnya
+def printSolusi(sol):
+    for i in sol:
+        for j in i:
+            print(str(j) + " ", end="")
+        print("")
+
+#fungsi untuk cek jika x,y valid
+#index for N*N maze
+def aman(maze, x, y):
+    if x >= 0 and x < N and y >= 0 and y < N and maze[x][y] == 1:
+        return True
+
+    return False
 
 
- # A utility function to check if x,y is valid
- # index for N*N Maze
-def isSafe(maze, x, y):
-         if x >= 0 and x < N and y >= 0 and y < N and maze[x][y] == 1:
-                 return True
 
-         return False
+def pecahkan(maze):
+    #matrix 8x8
+    sol = [[0 for j in range(8)] for i in range(8)]
 
+    if sebelumdipecahkan(maze, 0, 1, sol) == False:
+        print("Solusi tidak ada")
+        return False
 
-def solveMaze(maze):
-         # Creating a 4 * 4 2-D list
-         sol = [[0 for j in range(8)] for i in range(8)]
-
-         if solveMazeUtil(maze, 0, 0, sol) == False:
-                 print("Solution doesn't exist");
-                 return False
-
-         printSolution(sol)
-         return True
+    printSolusi(sol)
+    return True
 
 
- # A recursive utility function to solve Maze problem
-def solveMazeUtil(maze, x, y, sol):
-         # if (x,y is goal) return True
-         if x == N - 2 and y == N - 4:
-                 sol[x][y] = 1
-                 return True
+#fungsi untuk cek langkah
+def sebelumdipecahkan(maze, x, y, sol):
+    #titik koordinat mulai dan finish
+    if x == N - 2 and y == N - 4:
+        sol[x][y] = 1
+        return True
 
-         # Check if maze[x][y] is valid
-         if isSafe(maze, x, y) == True:
-                 # mark x, y as part of solution path
-                 sol[x][y] = 1
+    #cek jika maze[x][y] benar
+    if aman(maze, x, y) == True:
+    # tandai kalau kordinat benar
+        sol[x][y] = 1
 
-                 # Move forward in x direction
-                 if solveMazeUtil(maze, x + 1, y, sol) == True:
-                         return True
+        #ke bawah
+        if sebelumdipecahkan(maze, x + 1, y, sol) == True:
+            return True
 
-                 # If moving in x direction doesn't give solution
-                 # then Move down in y direction
-                 if solveMazeUtil(maze, x, y + 1, sol) == True:
-                         return True
+        #jika pindah kebawah ttidak bisa
+        #pindah kekanan
+        if sebelumdipecahkan(maze, x, y + 1, sol) == True:
+            return True
+        #jika ke kanan dan bawah tidak bisa
+        #pindah kiri
+        if sebelumdipecahkan(maze, x, y - 1, sol) == True:
+            return True
 
-                 if solveMazeUtil(maze, x, y - 1, sol) == True:
-                         return True
-                 # If none of the above movements work then
-                 # BACKTRACK: unmark x,y as part of solution path
-                 sol[x][y] = 0
-                 return False
+        sol[x][y] = 0
+        return False
+
+
 
 if __name__ == "__main__":
-         # Initialising the maze
-         maze = [[1,1,1,0,1,1,1,1],\
-       [1,1,0,0,0,1,1,0],\
-       [1,1,1,1,1,1,1,0],\
-       [1,1,1,1,1,1,1,0],\
-       [0,0,0,1,1,1,1,0],\
-       [1,1,1,0,0,1,1,0],\
-       [1,1,1,1,1,1,0,1],\
-       [1,1,1,1,0,0,1,1]]
 
-         solveMaze(maze)
+    maze = [0, 1, 1, 0, 1, 1, 1, 1],\
+           [1, 1, 0, 0, 0, 1, 1, 0],\
+           [1, 1, 1, 1, 1, 1, 1, 0],\
+           [1, 1, 1, 1, 1, 1, 1, 0],\
+           [0, 0, 0, 1, 1, 1, 1, 0],\
+           [1, 1, 1, 0, 0, 1, 1, 0],\
+           [1, 1, 1, 1, 1, 1, 0, 1],\
+           [1, 1, 1, 1, 0, 0, 1, 1]
+    pecahkan(maze)
